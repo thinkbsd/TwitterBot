858f69aafb640cc9208a8d7bf2e64d014e3a6ba2
Luke <66079471+thinkbsd@users.noreply.github.com>
2020-07-16 12:46:14 -0400

    Update DebtBot.py

--- DebtBot.py
+++ DebtBot.py
@@ -1,1 +1,10 @@
-# Twitter bot that will tweet the national debt daily.
+# Twitter bot that will tweet "Hello World!" (and eventually the national debt daily).
+from twython import Twython
+
+API_KEY = 'YOUR TWITTER API KEY'
+API_SECRET = 'YOUR TWITTER API SECRET'
+ACCESS_TOKEN = 'YOUR TWITTER ACCESS TOKEN'
+ACCESS_TOKEN_SECRET = 'YOUR TWITTER ACCESS TOKEN SECRET'
+
+twitter_client = Twython(API_KEY, API_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+twitter_client.update_status(status='Hello World!')
